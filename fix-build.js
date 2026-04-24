f8bac64a9d0fdbf360b594bb2cc0944a01cdb39d
Mounach <nihao34@ymail.com>
2026-04-24 18:53:05 +0200

    Active copie products json dans build Vercel

---
+++ fix-build.js
@@ -0,0 +1,5 @@
+﻿const fs = require("fs");
+const p = require("./package.json");
+p.scripts.build = "webpack --mode production && node scripts/copy-public.js";
+fs.writeFileSync("package.json", JSON.stringify(p, null, 2));
+console.log("BUILD =", p.scripts.build);
